f5b4e8c7b50ba0e8cc57f85bbdd6b201db31d683
saikarun99 <peddireddysaikarun@gmail.com>
2024-08-09 00:01:39 +0530

    making changes

--- 01.sample.sh
+++ 01.sample.sh
@@ -1,3 +1,9 @@
-This is a sample file for testing.
+#!/bin/bash
 
-echo "Hello world"
+#This is shebang notation, this tells the os with which scrit it has to be executed
+#There can only be one interpreter for one script and it should be first line 
+
+echo welcome to bash scripting
+
+#This is how we execute the script
+# bash scriptname.sh or ./scriptname.sh
