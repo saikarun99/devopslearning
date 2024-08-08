1c470f322e0afac6218e74a421974c00d21166e6
saikarun99 <peddireddysaikarun@gmail.com>
2024-08-09 00:55:07 +0530

    making changes

--- 03.colors.sh
+++ 03.colors.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+#echo -e "\e[color codem your msg to be printed\e[0m"
 echo -e "\e[33m I am printing yellow color \e[0m"
+
+#background + frontground
+
+#echo -e "\e[bgcode,foregroundcodem your msg to be printed\e[0m"
+echo -e "\e[43,34m hai hello world\e[0m"
