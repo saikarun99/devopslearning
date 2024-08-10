6b7d616dee874d1e4fee9674b1088f9068d75011
saikarun99 <peddireddysaikarun@gmail.com>
2024-08-10 17:43:54 +0530

    making changes

--- 10.conditions.sh
+++ 10.conditions.sh
@@ -6,15 +6,19 @@ action=$1
 case $action in 
 start)
 echo "starting payment service"
+exit 0
 ;;
 stop)
 echo "stopping payment service"
+exit 1
 ;;
 restart)
 echo "restarting payment service"
+exit 2
 ;;
 *)
 echo "Valid options are start or stop or restart"
 echo -e "Example usage: \n bash scriptname stop"
+exit 3
 ;;
 esac
