3745b4d1b55eaafbb1e5e3f8fae811c0047b5b5f
saikarun99 <peddireddysaikarun@gmail.com>
2024-08-10 21:27:09 +0530

    making changes

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -3,6 +3,16 @@
 #echo "I am frontend"
 set -e
 
+stat ( )
+{
+    if [$1 == 0]; then
+    echo "Successful"
+
+    else
+    echo "failed"
+    exit 2
+}
+
 USER_ID=$(id -u)
 
 if [ $USER_ID -ne 0 ]; then
@@ -11,26 +21,31 @@ fi
 
 echo "Installing nginx:"
 yum install nginx -y &>> /tmp/frontend.log
+stat $?
 
 echo "starting nginx"
 systemctl enable nginx &>> /tmp/frontend.log
 systemctl start nginx &>> /tmp/frontend.log
+stat $?
 
 echo "downloading the frontend file:"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip" 
-
+stat $?
 echo "cleanup of default frontend"
 cd /usr/share/nginx/html &>> /tmp/frontend.log
 rm -rf * &>> /tmp/frontend.log
+stat $?
 
 echo "extracting frontend"
 unzip /tmp/frontend.zip &>> /tmp/frontend.log
+stat $?
 
 echo "sorting the frontend files"
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat $?
 
 echo "restarting frontend"
 
