ee19530f212a5efe42704d3eeadf429416d5f87f
saikarun99 <peddireddysaikarun@gmail.com>
2024-08-10 17:41:08 +0530

    making changes

---
+++ 09. redirectors.sh
@@ -0,0 +1,7 @@
+#redirectors are of two types
+# Input redirection  (<) and output redirection (>) (>>) append the content to the latest existing content
+
+# 1> redirectors the output to another file
+# 2> redirects the error to another file.
+# &> redirects both error and output to another file.
+
---
+++ 10.conditions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# conditions are executed onlf if expression becomes true
+
+action = $1
+case $action in 
+start)
+echo "starting payment service"
+;;
+stop)
+echo "stopping payment service"
+;;
+restart)
+echo "restarting payment service"
+;;
+*)
+echo "Valid options are start or stop or restart"
+echo -e "Example usage: \n bash scriptname stop"
+;;
+esac
