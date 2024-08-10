277d099aa6be8cdd21d3911dcc4273d33774c3db
saikarun99 <peddireddysaikarun@gmail.com>
2024-08-10 21:16:16 +0530

    making changes

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
 
-echo "I am frontend"
+#echo "I am frontend"
+
+
+
+echo "Installing nginx:"
+yum install nginx -y &>> /tmp/frontend.log
+
+echo "starting nginx"
+systemctl enable nginx &>> /tmp/frontend.log
+systemctl start nginx &>> /tmp/frontend.log
+
+echo "downloading the frontend file:"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip" 
+
+echo "cleanup of default frontend"
+cd /usr/share/nginx/html &>> /tmp/frontend.log
+rm -rf * &>> /tmp/frontend.log
+
+echo "extracting frontend"
+unzip /tmp/frontend.zip &>> /tmp/frontend.log
+
+echo "sorting the frontend files"
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+echo "restarting frontend"
+
+systemctl daemon-reload
+systemctl restart nginx
+
