5b2ffefd09b57cf4c4b4347fd9375d7365d20e84
saikarun99 <peddireddysaikarun@gmail.com>
2024-08-10 18:08:43 +0530

    making changes

---
+++ 13. loops.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#loops are used to execute something for certain no of times
+
+# There are 2 major types of loops
+
+# For loop and while loop
+
+# When you know sth to be executed n no of times we use for loop
+
+# When you dont know sth to be executed how many times then while loop is used.
+
+# for loop syntax
+
+for val in 10 20 30 40 50 ; do
+
+echo "value for the loop is $val"
+
+done
