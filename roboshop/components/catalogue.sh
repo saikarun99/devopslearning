3fb19b0b777c3bb2d3d0feccc423b334264365c7
saikarun99 <peddireddysaikarun@gmail.com>
2024-08-11 19:01:41 +0530

    making changes

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -27,7 +27,7 @@ echo "configuring ${COMPONENT}"
 
 LOGFILE="/tmp/${COMPONENT}.log"
 echo -n "Installing nodeJs:"
-yum install https://rpm.nodesource.com/pub_16.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y &>> ${LOGFILE}
+#yum install https://rpm.nodesource.com/pub_16.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y &>> ${LOGFILE}
 yum install nodejs -y &>> ${LOGFILE}
 stat $?
 id ${APPUSER} &>> ${LOGFILE}
