71d874b572ac4f4da297debb03640aa11a76d086
saikarun99 <peddireddysaikarun@gmail.com>
2024-08-11 17:06:16 +0530

    making changes

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -6,7 +6,7 @@ set -e
 stat ()
 {
     if [ $1 == 0 ]; then
-    echo "Successful"
+    echo -e \e[32m "Successful" \e[0m
 
     else
     echo "failed"
@@ -24,31 +24,31 @@ echo -n "Installing nginx:"
 yum install nginx -y &>> /tmp/frontend.log
 stat $?
 
-echo "starting nginx"
+echo -n "starting nginx"
 systemctl enable nginx &>> /tmp/frontend.log
 systemctl start nginx &>> /tmp/frontend.log ; stat $?
 
 
-echo "downloading the frontend file:"
+echo -n "downloading the frontend file:"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip" 
 stat $?
-echo "cleanup of default frontend"
+echo -n "cleanup of default frontend"
 cd /usr/share/nginx/html &>> /tmp/frontend.log
 rm -rf * &>> /tmp/frontend.log
 stat $?
 
-echo "extracting frontend"
+echo -n "extracting frontend"
 unzip /tmp/frontend.zip &>> /tmp/frontend.log
 stat $?
 
-echo "sorting the frontend files"
+echo -n "sorting the frontend files"
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $?
 
-echo "restarting frontend"
+echo -n "restarting frontend"
 
 systemctl daemon-reload
 systemctl restart nginx
